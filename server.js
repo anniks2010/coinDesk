8c40d9f55d977806fcc3deb8a9d0bb4df7110099
Opilane TTHK <opilane@tthk.ee>
2020-09-23 09:35:34 +0300

    second commit

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const app=express();
 
 app.get('/bitcoinRates',(req,res)=>{
 let url= 'https://api.coindesk.com/v1/bpi/currentprice/eur.json';
-let rate='';
-let code='';
-let disclaimer='';
+
 
 axios.get(url)
     .then(function(response){
-      rate=response.data.bpi.EUR.rate;
-      code=response.data.bpi.EUR.code;
-      disclaimer=response.data.disclaimer;
+      let rate=response.data.bpi.EUR.rate;
+      let code=response.data.bpi.EUR.code;
+      let disclaimer=response.data.disclaimer;
       res.write(`<p>${rate} ${code}</p>`);
       res.write(`<p> ${disclaimer}</p>`);
       res.send();  
